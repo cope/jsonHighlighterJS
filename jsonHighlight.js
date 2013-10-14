740d83986918907eb071253092a47c00bbeb5453
Predrag Stojadinovic <predrag@stojadinovic.net>
2013-10-14 21:07:16 +0200

    reducing the namespace and adding options

--- jsonHighlight.js
+++ jsonHighlight.js
@@ -10,35 +10,43 @@
  */
 
 cope = ("undefined" === typeof cope) ? {} : cope;
-cope.json = ("undefined" === typeof cope.json) ? {} : cope.json;
-cope.json.Highlight = ("undefined" === typeof cope.json.Highlight) ? {} : cope.json.Highlight;
+cope.Highlight = ("undefined" === typeof cope.Highlight) ? {} : cope.Highlight;
 
-cope.json.Highlight.cssString = 'color: darkgreen;';
-cope.json.Highlight.cssNumber = 'color: darkorange;';
-cope.json.Highlight.cssBoolean = 'color: blue;';
-cope.json.Highlight.cssNull = 'color: magenta;';
-cope.json.Highlight.cssKey = 'color: red;';
+cope.Highlight.cssString = 'color: darkgreen;';
+cope.Highlight.cssNumber = 'color: darkorange;';
+cope.Highlight.cssBoolean = 'color: blue;';
+cope.Highlight.cssNull = 'color: magenta;';
+cope.Highlight.cssKey = 'color: red;';
 
-cope.json.Highlight.highlight = function (aJSON, aUseTab) {
+cope.Highlight.highlight = function (aJSON, aOptions) {
+	var indent = 2, useTabs = false;
+	if(aOptions) {
+		if(aOptions.indent) {
+			indent = aOptions.indent;
+		}
+		if(aOptions.useTabs) {
+			useTabs = aOptions.useTabs;
+		}
+	}
 	if (typeof aJSON != 'string') {
-		aJSON = JSON.stringify(aJSON, undefined, (aUseTab && aUseTab === true ? "\t" : 2));
+		aJSON = JSON.stringify(aJSON, undefined, (useTabs === true ? "\t" : indent));
 	} else {
-		aJSON = JSON.stringify(JSON.parse(aJSON), undefined, (aUseTab && aUseTab === true ? "\t" : 2));
+		aJSON = JSON.stringify(JSON.parse(aJSON), undefined, (useTabs === true ? "\t" : indent));
 	}
 
 	aJSON = aJSON.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 	return aJSON.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (aMatch) {
-		var lStyle = cope.json.Highlight.cssNumber;
+		var lStyle = cope.Highlight.cssNumber;
 		if (/^"/.test(aMatch)) {
 			if (/:$/.test(aMatch)) {
-				lStyle = cope.json.Highlight.cssKey;
+				lStyle = cope.Highlight.cssKey;
 			} else {
-				lStyle = cope.json.Highlight.cssString;
+				lStyle = cope.Highlight.cssString;
 			}
 		} else if (/true|false/.test(aMatch)) {
-			lStyle = cope.json.Highlight.cssBoolean;
+			lStyle = cope.Highlight.cssBoolean;
 		} else if (/null/.test(aMatch)) {
-			lStyle = cope.json.Highlight.cssNull;
+			lStyle = cope.Highlight.cssNull;
 		}
 		return '<span style="' + lStyle + '">' + aMatch + '</span>';
 	});
