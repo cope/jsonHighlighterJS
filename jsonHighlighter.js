a2799e78609f3b50712419ece54d3c69ee48b72c
Predrag Stojadinovic <predrag@stojadinovic.net>
2018-08-17 21:38:28 +0200

    fixing codacy issues

--- jsonHighlighter.js
+++ jsonHighlighter.js
@@ -21,24 +21,23 @@ var cope = {
 };
 
 cope.Highlighter.highlight = function (json, options) {
-	var indent = 2, tabs = false;
-	if (options) {
-		if (options.indent) indent = options.indent;
-		if (options.useTabs) tabs = options.useTabs;
-	}
-	if (typeof json != "string") json = JSON.stringify(json, null, (tabs === true ? "\t" : indent));
+	var indent = options.indent || 2,
+		tabs = true === options.useTabs;
+
+	if (typeof json !== "string") json = JSON.stringify(json, null, (tabs === true ? "\t" : indent));
 	else json = JSON.stringify(JSON.parse(json), null, (tabs === true ? "\t" : indent));
 
 	json = json.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-	return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-		var style = cope.Highlighter.cssNumber;
-		if (/^"/.test(match)) {
-			if (/:$/.test(match)) style = cope.Highlighter.cssKey;
-			else style = cope.Highlighter.cssString;
-		}
-		else if (/true|false/.test(match)) style = cope.Highlighter.cssBoolean;
-		else if (/null/.test(match)) style = cope.Highlighter.cssNull;
+	return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+		function (match) {
+			var style = cope.Highlighter.cssNumber;
+			if (/^"/.test(match)) {
+				if (/:$/.test(match)) style = cope.Highlighter.cssKey;
+				else style = cope.Highlighter.cssString;
+			}
+			else if (/true|false/.test(match)) style = cope.Highlighter.cssBoolean;
+			else if (/null/.test(match)) style = cope.Highlighter.cssNull;
 
-		return "<span style=\"" + style + "\">" + match + "</span>";
-	});
+			return "<span style=\"" + style + "\">" + match + "</span>";
+		});
 };
